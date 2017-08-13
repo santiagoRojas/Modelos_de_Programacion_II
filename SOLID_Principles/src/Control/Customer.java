84964cde857832397a8eb54226fc30f3f3478aa7
LeidyAldana <LeidyMarcelaAldana@gmail.com>
2017-08-13 14:44:51 -0500

    Launcher (the customer orders a coffee).

---
+++ SOLID_Principles/src/Control/Customer.java
@@ -0,0 +1,19 @@
+/*  Universidad Distrital Francisco José de Caldas 
+    Espacio académico: Modelos de Programación II
+    Docente: Alejandro Daza
+*/
+
+package Control;
+
+import View.Machine;
+
+public class Customer {    
+    
+    public static void order(){
+        Machine objectMachine=new Machine();       
+    }
+        
+    public static void main(String args[]){
+        order();        
+    }           
+}
